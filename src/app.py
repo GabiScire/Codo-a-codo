643284757d097656b4ca9e5da45941fd13574ff6
unknown <gabrielascire@gmail.com>
2021-08-25 20:07:15 -0300

    instancia-flask

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+import flask
+from flaskext.mysql import MySQL, Mysql
+
+app = flask(__name__)
+mysql = MySQL()
+
+if __name__ == '_main_':
+    app.run(debug=True)
